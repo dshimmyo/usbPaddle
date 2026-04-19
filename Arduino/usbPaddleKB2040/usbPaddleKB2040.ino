3edfc9503494b6483f6084d8cf812d37c3999cfe
Daniel Shimmyo <dshimmyo@gmail.com>
2026-04-19 18:51:12 -0400

    add rumble support

--- Arduino/usbPaddleKB2040/usbPaddleKB2040.ino
+++ Arduino/usbPaddleKB2040/usbPaddleKB2040.ino
@@ -17,6 +17,13 @@ Adafruit_USBD_HID usb_hid;
 #define BTN_SELECT 9
 #define BTN_START 10
 
+// Hardware Pin for the Rumble Motor (via transistor/MOSFET)
+#define MOTOR_PIN 2 // Use any PWM-capable pin
+
+// Global variables for rumble intensity
+uint8_t left_motor_val = 0;
+uint8_t right_motor_val = 0;
+
 void setup() {
   TinyUSBDevice.setManufacturerDescriptor("DKS Interactive LLC");
   TinyUSBDevice.setProductDescriptor("DKS Paddle v0");
@@ -34,8 +41,27 @@ void setup() {
   pinMode(BTN_B, INPUT_PULLUP);
   pinMode(BTN_SELECT, INPUT_PULLUP);
   pinMode(BTN_START, INPUT_PULLUP);
+  pinMode(MOTOR_PIN, OUTPUT);
 
   analogReadResolution(12);
+
+  // Set the callback for receiving data FROM the host (Mac/PC/Handheld)
+  usb_hid.setReportCallback(NULL, hid_out_report_cb);
+  analogWrite(MOTOR_PIN, 128);
+
+}
+
+// This function is called automatically whenever 
+// RetroArch or the OS sends a rumble command.
+void hid_out_report_cb(uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {
+  // Standard Gamepad Output Report is usually 2 or 4 bytes
+  // Format: [Strong Motor Intensity] [Weak Motor Intensity]
+  if (report_type == HID_REPORT_TYPE_OUTPUT) {
+    if (bufsize >= 2) {
+      left_motor_val = buffer[0];
+      right_motor_val = buffer[1];
+    }
+  }
 }
 
 void loop() {
@@ -60,6 +86,11 @@ void loop() {
   if (!digitalRead(BTN_B))      buttons |= (1 << 3);  // Trigger (3) - Usually 'Back' or 'A' in menu
   if (!digitalRead(BTN_SELECT)) buttons |= (1 << 9);  // Select (9)
   if (!digitalRead(BTN_START))  buttons |= (1 << 10); // Reset/Start (10)
+
+  // 3a. rumble:
+
+  uint8_t intensity = max(left_motor_val, right_motor_val);
+  analogWrite(MOTOR_PIN, intensity);
 
   // 4. Clean Report
   hid_gamepad_report_t report;
