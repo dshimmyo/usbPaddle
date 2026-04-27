8063971685427d239db2480213524c4002b9bfed
Daniel Shimmyo <dshimmyo@gmail.com>
2026-04-27 09:36:23 -0400

    clean out messy comments

--- Arduino/usbPaddleKB2040/usbPaddleKB2040.ino
+++ Arduino/usbPaddleKB2040/usbPaddleKB2040.ino
@@ -79,13 +79,6 @@ Adafruit_USBD_HID usb_hid;
 #define PADDLE_DPAD_HAT_LEFT      6
 #define PADDLE_DPAD_HAT_CENTERED  8
 
-// Hardware Pin for the Rumble Motor (via transistor/MOSFET)
-// #define MOTOR_PIN 2 // Use any PWM-capable pin
-
-// Global variables for rumble intensity
-// uint8_t left_motor_val = 0;
-// uint8_t right_motor_val = 0;
-
 void setup() {
   TinyUSBDevice.setManufacturerDescriptor("DKS Interactive LLC");
   TinyUSBDevice.setProductDescriptor("DKS Paddle v0");
@@ -113,23 +106,12 @@ void setup() {
 
   // Set the callback for receiving data FROM the host (Mac/PC/Handheld)
   usb_hid.setReportCallback(NULL, hid_out_report_cb);
-  // analogWrite(MOTOR_PIN, 255);
-  // delay(50);                   // Very short duration
-  // analogWrite(MOTOR_PIN, 0);
 }
 
-// This function is called automatically whenever 
-// RetroArch or the OS sends a rumble command.
 void hid_out_report_cb(uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {
   // Turn on the LED so we know the computer is actually sending data
   digitalWrite(LED_BUILTIN, HIGH); 
 
-  // if (report_type == HID_REPORT_TYPE_OUTPUT) {
-  //   if (bufsize >= 2) {
-  //     left_motor_val = buffer[0];
-  //     right_motor_val = buffer[1];
-  //   }
-  // }
 }
 
 typedef struct TU_ATTR_PACKED {
@@ -167,11 +149,6 @@ void loop() {
   if (!digitalRead(BTN_START))  buttons |= (1 << 10); // Anbernic Start
   if (!digitalRead(BTN_MENU))   buttons |= (1 << 11); // Anbernic Menu (Press)
   
-  // 3a. rumble:
-
-  // uint8_t intensity = max(left_motor_val, right_motor_val);
-  // analogWrite(MOTOR_PIN, intensity);
-
   // Clean Report
   dks_report_t report;//new struct
 
@@ -180,8 +157,7 @@ void loop() {
   report.y       = 0;       // 
   report.buttons = buttons;
   report.hat_byte = hat;
-  //report.hat_byte = (hat & 0x0F);
-  //report.hat_byte = (hat == PADDLE_DPAD_HAT_CENTERED) ? 0x08 : (hat & 0x0F);
+
   usb_hid.sendReport(1, &report, sizeof(report));// Explicitly ID 1
 
   // New Steam Deck Report (Report ID 2)
